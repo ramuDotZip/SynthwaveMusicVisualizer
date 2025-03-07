d8a909094087ae4d417f5c06f7169f5238f9a61d
Reina Quinn <edan.quinn@gmail.com>
2025-03-06 22:00:44 -0500

    Added manual override and made other tweaks

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -8,6 +8,7 @@ public class AudioController : MonoBehaviour
     [SerializeField] private AudioSource secondaryAudioSource;
     [SerializeField] float mix = 0.5f;
     [SerializeField] CSGFloatBuffer spectrumDataBuffer;
+    [SerializeField] OverrideLabelReader labelReader;
 
     public readonly float[] samplesMain = new float[2048];
     public readonly float[] samplesSecondary = new float[2048];
@@ -20,6 +21,8 @@ public class AudioController : MonoBehaviour
         for (int i = 0; i < 9; i++) {
             bandVolumes[i] = 0;
         }
+
+        labelReader.StartAnimation();
     }
 
     public void ResetAnimation() {
@@ -29,6 +32,8 @@ public class AudioController : MonoBehaviour
         for (int i = 0; i < 9; i++) {
             bandVolumes[i] = 0;
         }
+
+        labelReader.ResetAnimation();
     }
 
     void Update()
@@ -47,17 +52,22 @@ public class AudioController : MonoBehaviour
         for (int i = 0; i < 64; i++) {
             int position = i * (2 + i / 8);
             float value = samplesMain[position] * (1 - mix) + samplesSecondary[position] * mix;
-            value *= i + 1;
+            value *= (i + 1) * (i / 64 + 1);
             bandVolumes[8] += value;
             bandVolumes[i / 8] += value;
         }
 
         bandVolumes[8] /= 8;
+        float lrv = labelReader.GetCurrentValue();
+        bandVolumes[8] *= Mathf.Max(1 + lrv, 0.5f);
+        bandVolumes[8] += lrv * 0.8f + 0.3f;
         for (int i = 0; i < 9; i++) {
             float newVol = 1 - Mathf.Exp(-bandVolumes[i]);
             float oldVol = oldVols[i];
 
-            bandVolumes[i] = oldVol + Mathf.Clamp(newVol - oldVol, -(i == 8 ? 0.5f : 1.5f) * Time.deltaTime, (i == 8 ? 8 : 32) * Time.deltaTime);
+            //bandVolumes[i] = oldVol + Mathf.Clamp(newVol - oldVol, -(i == 8 ? 0.5f : 1.5f) * Time.deltaTime, 8 * (i == 8 ? 8 : 32) * Time.deltaTime);
+
+            bandVolumes[i] = Mathf.Lerp(newVol, oldVol, Mathf.Exp(-15 * Time.deltaTime)) + Mathf.Clamp(newVol - oldVol, 0, 4 * Time.deltaTime);
         }
 
         spectrumDataBuffer.Buffer.SetData(bandVolumes);
---
+++ Assets/Scripts/OverrideLabelReader.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverrideLabelReader : MonoBehaviour
+{
+    [SerializeField] TextAsset labels;
+    private string[] lines;
+    private int lineNumber;
+    private float currentSmoothedValue, currentEnvValue, envStartTime, envEndTime, envStartVal, envEndVal;
+
+    private float time = 0;
+    private bool playing = false;
+
+    void Start()
+    {
+        lines = labels.text.Split("\n");
+        ResetLineReader();
+    }
+
+    
+    void Update()
+    {
+        if (playing) time += Time.deltaTime;
+
+        if (time > envEndTime)
+        {
+            currentEnvValue = envEndVal;
+            ReadLine();
+        }
+        else if (time > envStartTime)
+        {
+            currentEnvValue = envStartVal + (envEndVal - envStartVal) * ((time - envStartTime) / (envEndTime - envStartTime));
+        }
+        else
+        {
+            currentEnvValue = 0;
+        }
+        currentSmoothedValue = Mathf.Lerp(currentEnvValue, currentSmoothedValue, Mathf.Exp(-15 * Time.deltaTime));
+    }
+
+    public float GetCurrentValue()
+    {
+        return currentSmoothedValue;
+    }
+
+    private void ResetLineReader()
+    {
+        lineNumber = 0;
+        ReadLine();
+    }
+
+    private void ReadLine()
+    {
+        
+        string currentLine;
+        string[] parts;
+        do
+        {
+            if (lineNumber >= lines.Length)
+            {
+                envStartTime = float.MaxValue;
+                envEndTime = float.MaxValue;
+                return;
+            }
+            currentLine = lines[lineNumber++];
+            parts = currentLine.Split(new string[] { "\t", " " }, StringSplitOptions.None);
+        } while (parts.Length < 4);
+        
+        envStartTime = float.Parse(parts[0]);
+        envEndTime = float.Parse(parts[1]);
+        envStartVal = float.Parse(parts[2]);
+        envEndVal = float.Parse(parts[3]);
+    }
+
+    public void StartAnimation()
+    {
+        time = 0;
+        playing = true;
+        ResetLineReader();
+    }
+
+    public void ResetAnimation()
+    {
+        time = 0;
+        playing = false;
+        ResetLineReader();
+    }
+
+}
